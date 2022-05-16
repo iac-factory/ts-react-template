4947f3cd99cf34e9f02939adaec01fa5fe7face8
Jacob B. Sanders <jacob.sanders@cloudhybrid.io>
2022-05-16 10:03:28 -0500

    Update

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -4,15 +4,12 @@ import Navigator from "./navigation";
 import Global from "./title";
 import Item from "./item";
 
-import { Dropdown } from "./dropdown";
-
 export const Menu = () => {
     return (
         <Navigator>
             <Global prefix={ "Vusion" } title={ "LLC" } reload={false}/>
             <Item title={ "Settings" } reload={false}/>
             <Item title={ "Mobile-Preview" } overwrite={"mobile-preview"} reload={false}/>
-            <Dropdown title={"Test"}/>
         </Navigator>
     );
 };
